093e7c325928f1f2a5ff6b1f7dae84abb749e824
sudharsanamoorthy <sudharsanamoorthy2003@gmail.com>
2025-01-10 08:04:10 +0530

    updateing

---
+++ script.js
@@ -0,0 +1,20 @@
+let progress=document.getElementById("progress");
+let song=document.getElementById("song");
+let ctrlIcon=document.getElementById("ctrlIcon");
+song.onloadedmetadata=function(){
+    progress.max=song.duration;
+    progress.value=song.currentTime;
+
+}
+function playPasue(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    }
+    else{
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
+}
